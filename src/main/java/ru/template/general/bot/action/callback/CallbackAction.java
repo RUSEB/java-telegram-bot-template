4d221869c9ae6048ccc87439bd32a023c257cc5f
Ruslan Gubaev <gub_ruslan@mail.ru>
2024-02-15 01:44:58 +0300

    Add TelegramIdByUpdateService to CallbackAction

--- src/main/java/ru/template/general/bot/action/callback/CallbackAction.java
+++ src/main/java/ru/template/general/bot/action/callback/CallbackAction.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.template.general.answer.service.AnswerService;
 import ru.template.general.answer.type.Answer;
+import ru.template.general.bot.service.TelegramIdByUpdateService;
 
 
 import java.util.HashSet;
@@ -11,9 +12,14 @@ import java.util.Set;
 
 public abstract class CallbackAction {
     protected AnswerService answerService;
+    protected TelegramIdByUpdateService telegramIdByUpdateService;
 
     protected Set<String> dataSet = new HashSet<>();
 
+    @Autowired
+    public void setTelegramIdByUpdateService(TelegramIdByUpdateService telegramIdByUpdateService){
+        this.telegramIdByUpdateService = telegramIdByUpdateService;
+    }
     @Autowired
     public void setAnswerService(AnswerService answerService){
         this.answerService = answerService;
